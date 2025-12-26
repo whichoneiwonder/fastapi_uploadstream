d3a61a27cdccd453d28474ea5881c25615d1d11c
whichoneiwonder <whichoneiwonder@github.com>
2025-12-27 02:02:01 +1100

    Refactor file structure and implement BinaryUploadFile and StreamBody for improved file handling

--- _demo.py
+++ _demo.py
@@ -1,22 +1,40 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = ["faststream"]
+# ///
 
-# Python
-from faststream.faststream import StreamBody, RequestBody
-from fastapi import FastAPI, UploadFile
 
+from faststream import StreamBody, BinaryUploadFile
+from fastapi import FastAPI, UploadFile, File, Depends
 
 
 app = FastAPI()
 
-@app.post("/items/")
-async def create_item(field: UploadFile = StreamBody(title="body content")) -> dict[str , str]:
+
+@app.post("/binary")
+async def upload_binary_content(
+    field: BinaryUploadFile = StreamBody(title="body content"),
+) -> dict[str, str]:
     return {
         "message": "Item created",
-        "content-type": field.content_type,
-        "body": (await field.body()).decode("utf-8")
+        "content-type": field.content_type or '',
+        "body": (await field.read(1024)).hex(),
     }
 
-if __name__ == '__main__':
+@app.post("/multipart")
+async def upload_multipart_file(
+    field: UploadFile = File(title="body content"),
+) -> dict[str, str]:
+    return {
+        "message": "Item created",
+        "content-type": field.content_type or '',
+        "body": (await field.read(1024)).hex(),
+    }
+
+
+if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(
         app,
         # reload=True,
